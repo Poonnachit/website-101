b621f13e7585c0eacefcf40a864767337448772b
Poonnachit <poonnachitam@gmail.com>
2022-09-13 14:46:04 +0700

    Lab day 13/09/2022

--- app.py
+++ app.py
@@ -1,27 +1,89 @@
-import mysql.connector
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, session
+from flaskext.mysql import MySQL
 from flask_cors import CORS
-
+import pymysql
 app = Flask(__name__)
 CORS(app)
+
 app.secret_key = "mysecretkey"
 
-conn = mysql.connector.connect(
-    host="localhost",
-    port=8889,
-    user="admin04",
-    password="admin004",
-    database="car_rental"
-)
-cursor = conn.cursor(dictionary=True)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 8889
+app.config['MYSQL_DATABASE_USER'] = 'admin04'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin004'
+app.config['MYSQL_DATABASE_DB'] = 'car_rental'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor(pymysql.cursors.DictCursor)
 
 
 @app.route("/")
 def home():
-    sql = "SELECT * FROM user"
-    cursor.execute(sql)
-    data = cursor.fetchall()
-    return render_template("index.html", users=data)
+    if "username" in session:
+        sql = "SELECT * FROM user"
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        name = session["name"]
+        email = session["email"]
+        return render_template("index.html", users=data, name=name, email=email)
+    return redirect(url_for("login"))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/action_login", methods=["POST"])
+def action_login():
+    if "username" in session:
+        return redirect(url_for("home"))
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        sql = "SELECT * FROM user WHERE username = %s AND password = %s"
+        cursor.execute(sql, (username, password))
+        data = cursor.fetchone()
+        if data:
+            session["username"] = data["username"]
+            session["name"] = data["name"]
+            session["tel"] = data["tel"]
+            session["email"] = data["email"]
+            flash("login successful")
+            return redirect(url_for("home"))
+        else:
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/action_register", methods=["POST"])
+def action_register():
+    if request.method == "POST":
+        firstname = request.form["first_name"]
+        lastname = request.form["last_name"]
+        name = firstname+" "+lastname
+        username = request.form["username"]
+        password = request.form["password"]
+        tel = request.form["tel"]
+        email = request.form["email"]
+        sql = "INSERT INTO user (name, username, password, tel, email) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(sql, (name, username, password, tel, email))
+        conn.commit()
+        flash("Register successful")
+        return redirect(url_for("login"))
 
 
 @app.route('/add_user', methods=['POST'])
